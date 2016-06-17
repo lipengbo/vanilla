16679e4c7deefd4aa83c2a324ffa86271adaa08e
Todd Burry <todd@vanillaforums.com>
2016-06-16 22:49:02 -0400

    Fix PHP 5.4 error in the SiteNaveModule

--- applications/dashboard/modules/class.sitenavmodule.php
+++ applications/dashboard/modules/class.sitenavmodule.php
@@ -281,7 +281,8 @@ class SiteNavModule extends NavModule {
             $currentSections = Gdn_Theme::section('', 'get');
             $currentSections = array_map('strtolower', $currentSections);
 
-            $hasCustomMenu = !empty(array_intersect(array_keys(self::$sectionItems), $currentSections));
+            $arr = array_intersect(array_keys(self::$sectionItems), $currentSections);
+            $hasCustomMenu = !empty($arr);
 
             if (!$hasCustomMenu) {
                 $currentSections = [self::SECTION_DEFAULT];
